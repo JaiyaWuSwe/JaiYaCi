1ce16195e57ee783c6b8aedb6ccd42435a0a307c
natza9301 <natlove.natlove202@gmail>
2018-11-15 22:04:22 +0700

    in this commit have 1.connect 2.controller Register 3.RegisterDto 4.RegisterDao 5.Filter (model mapper)

---
+++ jaiya/src/main/java/com/controller/Register.java
@@ -0,0 +1,65 @@
+package com.controller;
+
+import javax.print.attribute.standard.Fidelity;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.bson.Document;
+import org.modelmapper.ModelMapper;
+
+import com.connect.mongo.Connect;
+import com.dto.RegisterDto;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+
+@Path("/Register")
+public class Register {
+
+	@POST
+	@Path("/insert")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response insert(RegisterDto RegisterDto) {
+		// import mongodb
+		Connect mongo = new Connect();
+		MongoCollection<Document> collection = mongo.db.getCollection("user");
+		
+		//import json , modelmapper
+		JsonObject message = new JsonObject();
+		Gson gson = new Gson();
+		ModelMapper Mapper = new ModelMapper();
+		
+		BasicDBObject searchQuery =new BasicDBObject();
+		searchQuery.put("username", RegisterDto.getUsername());
+		
+		RegisterDto register = new RegisterDto();
+		
+		// if find return false else register user
+		try {
+			FindIterable<Document> data = collection.find(searchQuery);
+			register = Mapper.map(data.first(),RegisterDto.getClass());
+			message.addProperty("message", false);
+			
+		} catch (Exception e) {
+			message.addProperty("message", true);
+			String json = gson.toJson(RegisterDto);
+			Document document = Document.parse(json);
+			
+			try {
+				collection.insertOne(document);
+				message.addProperty("message", true);
+				
+			} catch (Exception error) {
+				message.addProperty("message", false);
+			}
+			return Response.ok(gson.toJson(message), MediaType.APPLICATION_JSON).build();
+			
+		}
+		return Response.ok(gson.toJson(message), MediaType.APPLICATION_JSON).build();
+	}
+}
---
+++ jaiya/src/main/java/com/dto/RegisterDto.java
@@ -0,0 +1,41 @@
+package com.dto;
+
+import org.bson.types.ObjectId;
+
+public class RegisterDto {
+	String username;
+	String password;
+	String email;
+	String tell;
+	String _id;
+	public ObjectId getId() {
+		return new ObjectId(_id);
+	}
+	public void setId(String _id) {
+		this._id = _id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTell() {
+		return tell;
+	}
+	public void setTell(String tell) {
+		this.tell = tell;
+	}
+}
