553f1041d2091da8e3fd2b6f0a8c4c56f17ed20e
natza9301 <natlove.natlove202@gmail>
2018-12-03 17:14:58 +0700

    timetogetpillow function

---
+++ jaiya/src/main/java/com/controller/TimeToGetPillow.java
@@ -0,0 +1,157 @@
+package com.controller;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.bson.Document;
+import org.modelmapper.ModelMapper;
+
+import com.connect.mongo.Connect;
+import com.dao.TimeToGetPillowDao;
+import com.dto.TimeToGetPillowDto;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+
+@Path("/timetogetpillow")
+public class TimeToGetPillow {
+	@POST
+	@Path("/insert")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response insert(TimeToGetPillowDto TimeToGetPillowDto) {
+		// import mongodb
+		Connect mongo = new Connect();
+		MongoCollection<Document> collection = mongo.db.getCollection("timetogetpillow");
+		
+		//import json , modelmapper
+		JsonObject message = new JsonObject();
+		Gson gson = new Gson();
+		ModelMapper Mapper = new ModelMapper();
+		
+		BasicDBObject searchQuery =new BasicDBObject();
+		searchQuery.put("time", TimeToGetPillowDto.getTime());
+		
+		TimeToGetPillowDto timeToGetPillowDto = new TimeToGetPillowDto();
+		
+		// if find return false else register user
+		try {
+			FindIterable<Document> data = collection.find(searchQuery);
+			timeToGetPillowDto = Mapper.map(data.first(),TimeToGetPillowDto.getClass());
+			message.addProperty("message", false);
+			
+		} catch (Exception e) {
+//			message.addProperty("message", true);
+			TimeToGetPillowDto.setStatus(1);
+			String json = gson.toJson(TimeToGetPillowDto);
+			Document document = Document.parse(json);
+			
+			try {
+				collection.insertOne(document);
+				message.addProperty("message", true);
+				
+			} catch (Exception error) {
+				message.addProperty("message", false);
+			}
+			return Response.ok(gson.toJson(message), MediaType.APPLICATION_JSON).build();
+			
+		}
+		return Response.ok(gson.toJson(message), MediaType.APPLICATION_JSON).build();
+	}
+	
+	@POST
+	@Path("/setAlertAgain")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response update(TimeToGetPillowDto TimeToGetPillowDto) {
+		Connect mongo = new Connect();
+		JsonObject message = new JsonObject();
+		Gson gson = new Gson();
+		MongoCollection<Document> collection = mongo.db.getCollection("timetogetpillow");
+		
+		TimeToGetPillowDao timeToGetPillowDao = new TimeToGetPillowDao();
+//
+		
+//		ReisterDao.setId(RegisterDto.getId());
+		timeToGetPillowDao.setStatus_alert(2);
+		
+		
+		
+		String json = gson.toJson(timeToGetPillowDao);
+		Document document = Document.parse(json);
+		
+		BasicDBObject setQuery = new BasicDBObject();
+        setQuery.put("$set", document);
+		
+		BasicDBObject searchQuery = new BasicDBObject();
+		searchQuery.put("_id", timeToGetPillowDao.get_id());
+		
+		try {
+			collection.updateOne(searchQuery, setQuery);
+			message.addProperty("message", true);
+		}catch (Exception e) {
+			message.addProperty("message", false);
+		}
+		
+		return Response.ok(gson.toJson(message), MediaType.APPLICATION_JSON).build();
+	}
+	@DELETE
+	@Path("/delete")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response delete(TimeToGetPillowDao TimeToGetPillowDao) {
+		Connect mongo = new Connect();
+		JsonObject message = new JsonObject();
+		Gson gson = new Gson();
+		MongoCollection<Document> collection = mongo.db.getCollection("timetogetpillow");
+		
+		try {
+			collection.deleteOne(Filters.eq("_id", TimeToGetPillowDao.get_id())); 
+			message.addProperty("message", true);
+		}catch (Exception e) {
+			message.addProperty("message", false);
+		}
+		
+		return Response.ok(gson.toJson(message), MediaType.APPLICATION_JSON).build();
+	}
+	
+	@POST
+	@Path("/changeAlert")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response changeAlert(TimeToGetPillowDto TimeToGetPillowDto) {
+		Connect mongo = new Connect();
+		JsonObject message = new JsonObject();
+		Gson gson = new Gson();
+		MongoCollection<Document> collection = mongo.db.getCollection("timetogetpillow");
+		
+		TimeToGetPillowDao timeToGetPillowDao = new TimeToGetPillowDao();
+//
+		
+//		ReisterDao.setId(RegisterDto.getId());
+		timeToGetPillowDao.setStatus(3);
+		
+		
+		
+		String json = gson.toJson(timeToGetPillowDao);
+		Document document = Document.parse(json);
+		
+		BasicDBObject setQuery = new BasicDBObject();
+        setQuery.put("$set", document);
+		
+		BasicDBObject searchQuery = new BasicDBObject();
+		searchQuery.put("_id", timeToGetPillowDao.get_id());
+		
+		try {
+			collection.updateOne(searchQuery, setQuery);
+			message.addProperty("message", true);
+		}catch (Exception e) {
+			message.addProperty("message", false);
+		}
+		
+		return Response.ok(gson.toJson(message), MediaType.APPLICATION_JSON).build();
+	}
+}
---
+++ jaiya/src/main/java/com/dto/TimeToGetPillowDto.java
@@ -0,0 +1,76 @@
+package com.dto;
+
+import org.bson.types.ObjectId;
+
+public class TimeToGetPillowDto {
+	String time;
+	String drung;
+	int among;
+	String volume;
+	String duration;
+	int alert;
+	int status_alert;
+	int status;
+	String userId;
+	String _id;
+	public ObjectId getId() {
+		return new ObjectId(_id);
+	}
+	public void setId(String _id) {
+		this._id = _id;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getDrung() {
+		return drung;
+	}
+	public void setDrung(String drung) {
+		this.drung = drung;
+	}
+	public int getAmong() {
+		return among;
+	}
+	public void setAmong(int among) {
+		this.among = among;
+	}
+	public String getVolume() {
+		return volume;
+	}
+	public void setVolume(String volume) {
+		this.volume = volume;
+	}
+	public String getDuration() {
+		return duration;
+	}
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+	public int getAlert() {
+		return alert;
+	}
+	public void setAlert(int alert) {
+		this.alert = alert;
+	}
+	public int getStatus_alert() {
+		return status_alert;
+	}
+	public void setStatus_alert(int status_alert) {
+		this.status_alert = status_alert;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+}
